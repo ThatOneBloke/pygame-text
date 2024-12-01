07c294cc6885223998c93f18d3f5d51cf1153fbf
ThatOneBloke <x1648811@gmail.com>
2024-12-01 16:57:30 +0000

    Add files via upload

---
+++ first.py
@@ -0,0 +1,8 @@
+import pgzrun
+def draw():
+    screen.clear()
+    screen.draw.text("welcome to pgzero", (300, 200), color = "green")
+    screen.draw.text("welcome to pgzero", (100, 50), color = "red")
+    screen.draw.text("welcome to pgzero", (325, 50), color = "blue")
+    screen.draw.text("welcome to pgzero", (500, 50), color = "yellow")
+pgzrun.go()
